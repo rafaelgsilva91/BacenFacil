fe9042a9e0ee2ac0866f345836637ddd7aae6149
Rafael Gouveia da Silva <desenvolvedor.rafael@gmail.com>
2019-12-28 22:03:10 +0000

    Ajustes dos campos

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
 import React, { PureComponent } from 'react';
 import AppContainer from './navigation/AppNavigator';
+import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
 
+const theme = {
+  ...DefaultTheme,
+  roundness: 2,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#3498db',
+    accent: '#f1c40f',
+  },
+};
 
 export default class App extends PureComponent {
     constructor(props) {
         super(props);
     }
     render() {
-        return <AppContainer />
+        return <PaperProvider theme={theme}><AppContainer /></PaperProvider>
     }
 }
--- app/CalculadoraRevisional.js
+++ app/CalculadoraRevisional.js
@@ -1,35 +1,95 @@
 import React, { Component } from 'react';
 import { Alert, Keyboard, Text, View, StyleSheet } from 'react-native';
 import { Constants } from 'expo';
-import { Formik } from 'formik';
-import { Button, TextInput, Appbar } from 'react-native-paper';
+import { Button, TextInput } from 'react-native-paper';
+import { Formik, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+
+const FormCalculadora = Yup.object({
+  taxaBacen: Yup.number()              
+  .required('Preencha esse campo'),
+  taxaContrato: Yup.number()              
+  .required('Preencha esse campo'),
+  vlrContrato: Yup.number()              
+  .required('Preencha esse campo'),
+  qdteParcela: Yup.number()              
+  .required('Preencha esse campo'),
+  vlrParcela: Yup.number()              
+  .required('Preencha esse campo'),
+});
 
 export default class CalculadoraRevisional extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Appbar.Header>
-          <Appbar.Content
-            title="Formik & React Native"
-          />
-        </Appbar.Header>
         <View style={styles.content}>
-          <Formik 
-            initialValues={{ firstName: '' }} 
+          <Formik    
+            initialValues={{ taxaBacen: '', taxaContrato: '', vlrContrato: '', qdteParcela: '', vlrParcela: '' }} 
+            validationSchema={FormCalculadora}
             onSubmit={values => {
                 Alert.alert(JSON.stringify(values, null, 2));
                 Keyboard.dismiss();
-              }
-            }>
-            {({ handleChange, handleSubmit, values }) => (
+            }}>
+            {({ handleChange, handleBlur, handleSubmit, touched, errors, values }) => (
               <View>
               <TextInput
-                onChangeText={handleChange('firstName')}
-                value={values.firstName}
-                label="First name"
-                placeholder="I am ready!"
+                onChangeText={handleChange('taxaBacen')}
+                onBlur={handleBlur('taxaBacen')}
+                value={values.taxaBacen}
+                style={styles.input}
+                label="Taxa Bacen"
+                placeholder="Informe a taxa do Bacen!"
               />
-              <Button onPress={handleSubmit} style={styles.button}>Submit</Button>
+              {touched.taxaBacen && errors.taxaBacen ? (
+                <Text style={styles.error}>{errors.taxaBacen}</Text>
+              ) : null}
+              <TextInput
+                onChangeText={handleChange('taxaContrato')}
+                onBlur={handleBlur('taxaContrato')}
+                value={values.taxaContrato}
+                style={styles.input}
+                label="Taxa do Contrato"
+                placeholder="Informe a taxa do contrato!"
+              />
+              {touched.taxaContrato && errors.taxaContrato ? (
+                <Text style={styles.error}>{errors.taxaContrato}</Text>
+              ) : null}
+              <TextInput
+                onChangeText={handleChange('vlrContrato')}
+                onBlur={handleBlur('vlrContrato')}
+                value={values.vlrContrato}
+                style={styles.input}
+                label="Valor do Contrato"
+                placeholder="Informe o valor do contrato!"
+              />
+              {touched.vlrContrato && errors.vlrContrato ? (
+                <Text style={styles.error}>{errors.vlrContrato}</Text>
+              ) : null}
+              <TextInput
+                onChangeText={handleChange('qdteParcela')}
+                onBlur={handleBlur('qdteParcela')}
+                value={values.qdteParcela}
+                style={styles.input}
+                label="Qtde. de Parcelas"
+                placeholder="Informe a qtde. parcelas!"
+              />
+              {touched.qdteParcela && errors.qdteParcela ? (
+                <Text style={styles.error}>{errors.qdteParcela}</Text>
+              ) : null}
+              <TextInput
+                onChangeText={handleChange('vlrParcela')}
+                onBlur={handleBlur('vlrParcela')}
+                value={values.vlrParcela}
+                style={styles.input}
+                label="Valor da Parcela"
+                placeholder="Informe o valor da parcela!"
+              />
+              {touched.vlrParcela && errors.vlrParcela ? (
+                <Text style={styles.error}>{errors.vlrParcela}</Text>
+              ) : null}
+              <Button onPress={handleSubmit} style={styles.button}>
+                Calcular
+              </Button>
               </View>
             )}
           </Formik>
@@ -42,12 +102,25 @@ export default class CalculadoraRevisional extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#ecf0f1',
   },
   content: {
     padding: 16,
   },
   button: {
     marginTop: 16,
-  }
+  },
+  input: {
+    height: 50,
+    paddingHorizontal: 8,
+    width: '100%',
+    borderColor: '#CCC',
+    borderWidth: 1,
+    backgroundColor: '#fff',
+  },
+  error: {
+    margin: 8,
+    fontSize: 14,
+    color: 'red',
+    fontWeight: 'bold',
+  },
 });
