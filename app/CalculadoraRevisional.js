bedc254ca9fb1bbe806a14d053e6235f3f221011
Rafael Gouveia da Silva <desenvolvedor.rafael@gmail.com>
2019-12-28 05:36:24 +0000

    Testando Formularios com Formik

--- app/CalculadoraRevisional.js
+++ app/CalculadoraRevisional.js
@@ -1,85 +1,53 @@
-import React from "react";
-import { SafeAreaView, StyleSheet, Text, View} from "react-native";
-import t from 'tcomb-form-native';
+import React, { Component } from 'react';
+import { Alert, Keyboard, Text, View, StyleSheet } from 'react-native';
+import { Constants } from 'expo';
+import { Formik } from 'formik';
+import { Button, TextInput, Appbar } from 'react-native-paper';
 
-//Configuração do formulário
-const Form = t.form.Form;
-
-const User = t.struct({
-  email: t.String,
-  username: t.maybe(t.String),
-  password: t.String,
-  terms: t.Boolean
-});
-
-const formStyles = {
-  ...Form.stylesheet,
-  formGroup: {
-    normal: {
-      marginBottom: 10
-    },
-  },
-  controlLabel: {
-    normal: {
-      color: 'blue',
-      fontSize: 18,
-      marginBottom: 7,
-      fontWeight: '600'
-    },
-    error: {
-      color: 'red',
-      fontSize: 18,
-      marginBottom: 7,
-      fontWeight: '600'
-    }
-  }
-}
-
-const options = {
-  fields: {
-    email: {
-      error: 'Without an email address how are you going to reset your password when you forget it?'
-    },
-    password: {
-      error: 'Choose something you use on a dozen other sites or something you won\'t remember'
-    },
-    terms: {
-      label: 'Agree to Terms',
-    },
-  },
-  stylesheet: formStyles,
-};
-
-export default class CalculadoraRevisional extends React.Component {
-
-  constructor(props) {
-    super(props);     
-  }
-
-  componentDidMount() {    
-         
-  }
-
-  render() {   
+export default class CalculadoraRevisional extends Component {
+  render() {
     return (
       <View style={styles.container}>
-        <Form 
-          ref={c => this._form = c}
-          type={User} 
-          options={options}
-        />
-        <Button
-          title="Sign Up!"
-          onPress={this.handleSubmit}
-        />
+        <Appbar.Header>
+          <Appbar.Content
+            title="Formik & React Native"
+          />
+        </Appbar.Header>
+        <View style={styles.content}>
+          <Formik 
+            initialValues={{ firstName: '' }} 
+            onSubmit={values => {
+                Alert.alert(JSON.stringify(values, null, 2));
+                Keyboard.dismiss();
+              }
+            }>
+            {({ handleChange, handleSubmit, values }) => (
+              <View>
+              <TextInput
+                onChangeText={handleChange('firstName')}
+                value={values.firstName}
+                label="First name"
+                placeholder="I am ready!"
+              />
+              <Button onPress={handleSubmit} style={styles.button}>Submit</Button>
+              </View>
+            )}
+          </Formik>
+        </View>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  text: {
-    fontSize: 16,   
-    color: "#333333"
+  container: {
+    flex: 1,
+    backgroundColor: '#ecf0f1',
+  },
+  content: {
+    padding: 16,
+  },
+  button: {
+    marginTop: 16,
   }
 });
